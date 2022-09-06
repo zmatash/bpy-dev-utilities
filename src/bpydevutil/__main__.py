2b8385737e996f376f18e2fde25d912445baad99
MattA <m.d.ashpole@gmail.com>
2022-09-06 01:22:26 +0100

    Feat: Implement pack tool CLI interface.

--- src/bpydevutil/__main__.py
+++ src/bpydevutil/__main__.py
@@ -5,8 +5,9 @@ import iniconfig
 import typer
 from iniconfig import IniConfig
 
-from common import get_addon_srcs
+import common
 from install import InstallAddonsFromSource
+from pack import PackAddonsFromSource
 
 app = typer.Typer()
 
@@ -47,6 +48,21 @@ def parse_cfg_list(value: str) -> Union[list[str], None]:
     return value.split(",")
 
 
+def check_directories(directory_params: dict[str, str]) -> None:
+    """Check given directory arguments exist and are valid.
+
+    Args:
+        directory_params: Dictionary of arguments and corresponding values..
+
+    """
+    for param in directory_params.keys():
+        if not directory_params[param]:
+            raise typer.BadParameter(f"<{param}> cannot be empty.")
+
+        if not Path(directory_params[param]).is_dir():
+            raise typer.BadParameter(f"<{param}> <{directory_params[param]}> is not an existing directory.")
+
+
 @app.command()
 def install(
     src_dir: str = typer.Argument(
@@ -66,18 +82,42 @@ def install(
 
     directory_params = {"src_dir": src_dir, "blender_addons_dir": blender_addons_dir}
 
-    for param in directory_params.keys():
-        if not directory_params[param]:
-            raise typer.BadParameter(f"<{param}> cannot be empty.")
-
-        if not Path(directory_params[param]).is_dir():
-            raise typer.BadParameter(f"<{param}> <{directory_params[param]}> is not an existing directory.")
+    check_directories(directory_params)
 
     install_tool = InstallAddonsFromSource(Path(src_dir), Path(blender_addons_dir), excluded_addons)
 
-    addon_srcs = get_addon_srcs(install_tool.excluded_addons, install_tool.addons_src)
+    addon_srcs = common.get_addon_srcs(install_tool.addons_src, install_tool.excluded_addons)
     install_tool.clear_old_addons([path.name for path in addon_srcs])
+    for addon in addon_srcs:
+        common.clear_unused_files(addon)
     install_tool.install_addons(addon_srcs)
+
+
+@app.command()
+def pack(
+    src_dir: str = typer.Argument(
+        default=get_cfg_arg(ini_config, "src_dir"), help="Directory where addon sources are located."
+    ),
+    release_dir: str = typer.Argument(
+        default=get_cfg_arg(ini_config, "release_dir"), help="Directory where addons should be moved after packing."
+    ),
+    excluded_addons: Optional[list[str]] = typer.Option(
+        default=parse_cfg_list(get_cfg_arg(ini_config, "excluded_addons")),
+        help="Addon names to be excluded from installion, separate names with commas.",
+    ),
+):
+    directory_params = {"src_dir": src_dir, "release_dir": release_dir}
+
+    check_directories(directory_params)
+
+    packing_tool = PackAddonsFromSource(Path(src_dir), Path(release_dir), excluded_addons)
+    addon_srcs = common.get_addon_srcs(packing_tool.addons_src, packing_tool.excluded_addons)
+
+    for addon in addon_srcs:
+        bl_info = packing_tool.get_addon_data(addon)
+        name = packing_tool.generate_zip_name(bl_info)
+        common.clear_unused_files(addon, remove_suffixes)
+        packing_tool.pack_addon(addon, name)
 
 
 if __name__ == "__main__":
