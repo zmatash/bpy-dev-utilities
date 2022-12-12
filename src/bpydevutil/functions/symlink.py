52da0fb5ac8ce486a1f994d0d703306043457b7b
MattA <m.d.ashpole@gmail.com>
2022-12-12 06:16:37 +0000

    Fix: addon -> addon_path

--- src/bpydevutil/functions/symlink.py
+++ src/bpydevutil/functions/symlink.py
@@ -4,25 +4,25 @@ from pathlib import Path
 
 
 class SymlinkToAddonSource:
+    """Symlink creator."""
     def __init__(self, addons_install_dir: Path) -> None:
         """
         Args:
             addons_install_dir: Directory to create symlinks in.
-
         """
         self.addons_install_dir = addons_install_dir
 
-    def create_sympath(self, addon: Path) -> Path:
-        """Create a sympath to the addon source inside the addons installation folder.
+    def create_symlink(self, addon_path: Path) -> Path:
+        """Create a symlink to the addon source inside the addons installation folder.
 
         Args:
-            addon: Source directory of the addon to create a symlink to.
+            addon_path: Source directory of the addon to create a symlink to.
 
         Returns:
-            The sympath directory.
+            The symlink directory.
         """
 
-        sympath = Path(self.addons_install_dir / addon.name)
-        sympath.symlink_to(addon)
+        symlink = Path(self.addons_install_dir / addon_path.name)
+        symlink.symlink_to(addon_path)
 
-        return sympath
+        return symlink
